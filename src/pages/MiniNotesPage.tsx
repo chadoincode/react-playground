b169304bc32d0d36fe954599687a0c6ee047a7fc
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-04-11 01:03:35 +0700

    style: update style mini notes card

--- src/pages/MiniNotesPage.tsx
+++ src/pages/MiniNotesPage.tsx
@@ -8,9 +8,9 @@ type NoteProps = {
 
 export const MiniNotesCard =  ({id, text}: NoteProps) => {
   return(
-    <div className="">
-      <h3>{text}</h3>
-      <button>Delete</button>
+    <div className="flex bg-accent rounded-2xl p-3 justify-between gap-3 align-center items-center">
+      <h3 className="font-medium text-2xl">{text}</h3>
+      <button className="p-2">Delete</button>
     </div>
   )
 }
@@ -21,7 +21,8 @@ const MiniNotesApps = () => {
 
   const handleSubmit = () => {
     if(input !== ""){
-      return setNotes([...notes, {id: notes.length + 1, text: input}])
+      setNotes([...notes, {id: notes.length + 1, text: input}])
+      setInput("")
     } else {
       alert("Input can't be blank")
     }
@@ -30,6 +31,7 @@ const MiniNotesApps = () => {
     <div>
       <Header title="Mini Notes App"/>
       <input 
+        value={input}
         type="text" 
         onChange={(e) => setInput(e.target.value)}
         className="border-b-gray-50 bg-light rounded-lg p-1.5 mr-2"
@@ -38,13 +40,15 @@ const MiniNotesApps = () => {
         onClick={handleSubmit}
         className="p-2 text-sm bg-light cursor-pointer rounded-lg"
       >Submit</button>
-      {notes.map((note) =>(
-        <MiniNotesCard 
-          key={note.id}
-          id={note.id}
-          text={note.text}
-        />
-      ))}
+      <div className="flex flex-col gap-3 mt-7">
+        {notes.map((note) =>(
+          <MiniNotesCard 
+            key={note.id}
+            id={note.id}
+            text={note.text}
+          />
+        ))}
+      </div>
     </div>
   )
 }
